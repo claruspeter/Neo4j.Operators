a069225911741f1065aa16341630369e66bb09f6
peterb <prbayne@gmail.com>
2017-05-23 16:45:06 +1200

    Can bind to ONE resturn value for now.

--- Neo4j.Operators.fs
+++ Neo4j.Operators.fs
@@ -1,34 +1,28 @@
 module Neo4j.Operators
 
 open System
+open System.Collections.Generic
 open System.Linq
 open System.Linq.Expressions
 open Microsoft.FSharp.Quotations
 open Microsoft.FSharp.Linq.RuntimeHelpers
 open Neo4jClient
 
-
-type private FunAs() =
-    static member LinqExpression1<'TResult>(e: Expression<Func<'TResult>>) = e
-    static member LinqExpression2<'T, 'TResult>(e: Expression<Func<'T, 'TResult>>) = e
-    static member LinqExpression3<'T, 'R, 'TResult>(e: Expression<Func<'T, 'R, 'TResult>>) = e
-    static member LinqExpression4<'T, 'R, 'U, 'TResult>(e: Expression<Func<'T, 'R, 'U, 'TResult>>) = e
-
-type ExpressionNode< ^a when ^a:(static member NAME:string)> 
+type ExpressionNode< 'a> 
     = { v:string }
     with static member inline Init<'a> varName : ExpressionNode<'a> = {v=varName }
 
-type ExpressionRel< ^a when ^a:(static member NAME:string)> 
+type ExpressionRel< 'a > 
     = { v:string }
     with static member inline Init<'a> varName : ExpressionRel<'a> = {v=varName }
 
 type ExpressionNodeRel = { lhs: string; }
 
-let inline ExprNodeName< ^b when ^b:(static member NAME:string)> (x: ExpressionNode< ^b > ) =
-    sprintf "(%s:%s)" x.v ( ^b:(static member NAME:string) () )
+let ExprNodeName<'a> (x: ExpressionNode< 'a > ) =
+    sprintf "(%s:%s)" x.v ( x.GetType().GenericTypeArguments.[0].Name )
 
-let inline ExprRelName< ^b when ^b:(static member NAME:string)> (x: ExpressionRel< ^b > ) =
-    sprintf "%s:%s" x.v ( ^b:(static member NAME:string) () )
+let ExprRelName<'a> (x: ExpressionRel< 'a > ) =
+    sprintf "%s:%s" x.v ( x.GetType().GenericTypeArguments.[0].Name )
 
 let inline (@.@) (ev:ExpressionNode<'b>) (prop:string) =
     sprintf "%s.%s" (ev.v) prop
@@ -51,10 +45,28 @@ let inline ( <-| ) (a:ExpressionNodeRel) (b:ExpressionRel<'a>) =
 let inline ( |- ) (a:ExpressionNodeRel) (b:ExpressionNode<'b>) =
     { lhs = sprintf "%s]-%s" a.lhs (ExprNodeName b) }
 
+type private FunAs() =
+    static member LinqExpression1<'TResult>(e: Expression<Func<'TResult>>) = e
+    static member LinqExpression2<'T, 'TResult>(e: Expression<Func<'T, 'TResult>>) = e
+    static member LinqExpression3<'T, 'R, 'TResult>(e: Expression<Func<'T, 'R, 'TResult>>) = e
+    static member LinqExpression4<'T, 'R, 'U, 'TResult>(e: Expression<Func<'T, 'R, 'U, 'TResult>>) = e
+
+
+
 
 
 type Cypher.ICypherFluentQuery with
-    member inline this.Match< ^b when ^b:(static member NAME:string)> (x: ExpressionNode< ^b > ) =
+    member this.Match x =
         this.Match( ExprNodeName x )
-    member inline this.Match (x: ExpressionNodeRel ) =
+    member this.Match x =
         this.Match( x.lhs )
+    member this.Return<'a> (x: ExpressionNode<'a>  ) =
+        // let para = [| ParameterExpression.Parameter( typeof<Cypher.ICypherResultItem>, x.v ) |]
+        // let body = Expression.Variable(typeof< 'a>, x.v)
+        // let mthd = MethodCallExpression.Lambda( body, para )
+        // //let q = Expr.                                  //.Lambda( a, <@@ a @@> ) :?> Expr<'a>
+        // //let aExpr = LeafExpressionConverter.QuotationToLambdaExpression mthd
+        // let aCompiled = unbox<Expression<Func<'a>>> mthd
+        this.Return<'a>(x.v)
+
+
--- Script.fsx
+++ Script.fsx
@@ -5,9 +5,9 @@
 #r "Neo4j.Operators"
 #r "Neo4jClient"
 
-using System
-using Neo4jClient
-using Neo4j.Operators
+open System
+open Neo4jClient
+open Neo4j.Operators
 
 [<Literal>]
 let connectionstring = @"http://localhost:7474/db/data"
@@ -15,4 +15,31 @@ let connectionstring = @"http://localhost:7474/db/data"
 let user = "neo4j" 
 [<Literal>]
 let pwd = "password"
-let db = new Neo4jClient.GraphClient(Uri(connectionstring),user ,pwd)
+let db = 
+    let db = new Neo4jClient.GraphClient(Uri(connectionstring),user ,pwd)
+    db.Connect()
+    db
+
+[<CLIMutable>]
+type Person = { name: string; born: int;}
+[<CLIMutable>]
+type Movie = { name: string; released: int; }
+[<CLIMutable>]
+type ACTED_IN = {a:string}
+[<CLIMutable>]
+type DIRECTED = {b:string}
+
+
+let writer = ExpressionNode<Person>.Init "writer" 
+let director = ExpressionNode<Person>.Init "director" 
+let movie = ExpressionNode<Movie>.Init "m" 
+let wrote = ExpressionRel<ACTED_IN>.Init "w" 
+let directed = ExpressionRel<DIRECTED>.Init "d" 
+
+//let exWisD = fun (writer: Cypher.ICypherResultItem) (m: Cypher.ICypherResultItem) -> (writer.As<schema.Person.Proxy>(), m.As<schema.Movie.Proxy>()) 
+db.Cypher
+    .Match(  writer -| wrote |-> movie <-| directed |- writer  )   
+    .Return( writer )
+    .Limit(Nullable<int>(20))
+    .Results
+    |> Seq.toList
